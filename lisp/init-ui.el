e33aec1707c1baab8144d6ec31936d3b18f6f4c1
Your Name <you@example.com>
2025-08-26 15:52:43 +0800

    upload the phara in ui-config

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -234,7 +234,7 @@
   :config
   (setq dashboard-startup-banner "~/.emacs.d/girl.jpg") ;; 也可以自定义图片
   (setq dashboard-image-banner-max-height 300)  ;; 最大高度 (像素)
-  (setq dashboard-banner-logo-title "Welcome to Emacs!") ;; 个性签名，随读者喜好设置
+  (setq dashboard-banner-logo-title "Welcome to Emacs Code Space!") ;; 个性签名，随读者喜好设置
   ;; (setq dashboard-projects-backend 'projectile) ;; 读者可以暂时注释掉这一行，等安装了 projectile 后再使用
   (setq dashboard-items '((recents  . 5)   ;; 显示多少个最近文件
                           (bookmarks . 5)  ;; 显示多少个最近书签
